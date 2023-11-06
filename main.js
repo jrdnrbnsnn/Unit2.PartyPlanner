12f913cdd32a638d3c33cf82e6aecb3a34adad3a
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2023-11-05 20:38:14 -0600

    Needs work

---
+++ main.js
@@ -0,0 +1,75 @@
+const COHORT = "2308-acc-et-web-pt-a";
+
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
+
+const state = {
+  events: [],
+};
+
+const eventList = document.querySelector("#events");
+
+const addEventForm = document.querySelector("#addEvent");
+addEventForm.addEventListener("submit", addEvent);
+
+async function render() {
+  await getEvents();
+  renderEvents();
+}
+
+render();
+
+async function getEvents() {
+  try {
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    state.events = json.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderEvents() {
+  // TODO
+  if (!state.events.length) {
+    eventList.innerHTML = "<li>No artists.</li>";
+    return;
+  }
+
+  const eventCards = state.events.map((event) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <h2>${event.name}</h2>
+        <p>${event.description}</p>
+        <p>${event.date}</p>
+        <p>${event.location}</p>
+      `;
+    return li;
+  });
+
+  eventList.replaceChildren(...eventCards);
+}
+
+async function addEvent(event) {
+  event.preventDefault();
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: addEventForm.name.value,
+        description: addEventForm.description.value,
+        date: addEventForm.date.value,
+        location: addEventForm.location.value,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create event");
+    }
+
+    render();
+  } catch (error) {
+    console.error(error);
+  }
+}
